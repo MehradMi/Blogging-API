6fccc7aa16055c44011111f6a4144900ece6f460
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-06-28 18:45:05 +0330

    added simple post

--- app/main.py
+++ app/main.py
@@ -1,4 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+class BlogPost(BaseModel):
+	title: str
+	content: str
+	category: str
+	tags: list[str]
+
+class BlogPostResponse(BaseModel):
+	id: int
+	title: str
+	content: str
+	category: str
+	tags: list[str]
+	createdAt: datetime
+	updatedAt: datetime
 
 app = FastAPI()
 
+posts = []
+
+@app.post("/posts/", status_code=201)
+async def create_blog_post(blog_post: BlogPost):
+
+	new_post = {
+		"id": len(posts) + 1,
+		**blog_post.model_dump(),
+		"createdAt": datetime.now(),
+		"updatedAt": datetime.now()
+	}
+
+	posts.append(new_post)
+
+	return new_post
